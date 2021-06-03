57a776e18159b77eb9c02562b06ec473020f1bfb
Loranne Nasir <loranne.n@gmail.com>
2021-06-03 11:05:36 -0700

    pick question function handles empty list of unused questions

--- magic_hat.py
+++ magic_hat.py
@@ -2,12 +2,11 @@
 # read in as CSV?
 import csv 
 import random
-import time
-import sys
 import select
+import sys
+import time
 
 # initialize empty dict, to be populated by reading in CSV
-
 def populate_questions():
     """Populates dictionary of questions, IDs, and used values (default to false) 
     using data from CSV file"""
@@ -47,9 +46,22 @@ def show_menu():
     return play_mode
 
 
-def pick_question():
+def pick_question(questions, unused_qids):
     """Chooses a random question for the team based on list of unused questions.
     Returns question text."""
+
+    def are_unused(unused_qids):
+        """Checks whether list of unused Qs is empty"""
+
+        return len(unused_qids)
+
+    # while len of unused_qids is 0 (meaning there are no unused Qs)
+    while not are_unused(unused_qids):
+        # loop over keys in questions to add them all back to unused list
+        for key in questions:
+            unused_qids.append(key)
+            # update used status to False
+            questions[key]["used"] = False
 
     # pick a random ID from list of unused qs
     q_id = random.choice(unused_qids)
@@ -60,11 +72,12 @@ def pick_question():
     # remove ID from list of unused qs
     unused_qids.remove(q_id)
 
-    # show_menu()
+    # change used state to True
+    questions[q_id]["used"] = True
 
     # return question text
     return ask_q
-    #TODO: handle what happens when unused_qids is empty
+    #DONE: handle what happens when unused_qids is empty
 
 
 def get_periodic_questions():
@@ -80,6 +93,7 @@ def get_periodic_questions():
         interval = input()
     
     # convert time to seconds for use with sleep()
+    # TODO: uncomment line 97
     # interval = interval * 60
 
     # helper function for validating user input during continuous questions
@@ -90,7 +104,7 @@ def get_periodic_questions():
 
     # loop for printing questions
     while True:
-        print(pick_question())
+        print(pick_question(questions, unused_qids))
         print("Press 'X' and then 'Return' any time to stop getting questions.\n")
         # wait however long user said
         time.sleep(interval)
@@ -121,7 +135,7 @@ print("Welcome to Magic Hat, the icebreaker question game. Gather your team and 
 play_mode = show_menu()
 
 if play_mode == "a":
-    print(pick_question())
+    print(pick_question(questions, unused_qids))
 elif play_mode == "b":
     print(get_periodic_questions())
 elif play_mode == "q":
